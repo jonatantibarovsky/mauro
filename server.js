cfc843dc0f10bd988b11b48ae3dc6059ca30d5fc
Jonatan Tibarovsky <jonatantibarovsky@Jonatans-MacBook-Pro.local>
2021-02-01 19:04:57 +0100

    ssl

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const https = require('https')
+const fs = require('fs')
 const app = express();
+
+const privateKey = fs.readFileSync('./myCA.key')
+const certificate = fs.readFileSync('./myCA.pem')
 
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -13,3 +18,9 @@ app.get('/*', function (req, res) {
 http.createServer(app).listen(80);
 
 console.log('server is running')
+
+https.createServer({
+  key: privateKey,
+  cert: certificate,
+  passphrase: '0213456789ABC'
+}, app).listen(443)
