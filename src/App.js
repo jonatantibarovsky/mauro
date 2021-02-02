c823b5a6f4491ceda6f584557c4291a3fa23bf6b
Jonatan Tibarovsky <jonatantibarovsky@Jonatans-MacBook-Pro.local>
2021-02-02 13:22:36 +0100

    small fix

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ const App = () => {
         showMenu={showMenu}
         setShowMenu={setShowMenu}
       />
-      {banner &&  <Banner data={banner}/>}
+      {banner && banner.bekapcsolva && <Banner data={banner}/>}
       <div className={classes.backgroundImage}>
         <LandingSection
           language={language}
